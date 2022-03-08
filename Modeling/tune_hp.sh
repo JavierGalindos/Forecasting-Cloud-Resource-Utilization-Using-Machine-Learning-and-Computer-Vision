1f9f993ac477f2a81374f984cf3b81e20f605097
javier.galindos <javier.galindos@imdea.org>
2022-03-08 12:12:49 +0100

    Develop many to many LSTM

--- Modeling/tune_hp.sh
+++ Modeling/tune_hp.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-python3 LSTM_train.py -e 100 -n 'neurons_20' -d 20 -s 'layers'
-python3 LSTM_train.py -e 100 -n 'neurons_50' -d 50 -s 'neurons'
-python3 LSTM_train.py -e 100 -n 'neurons_10' -d 10 -s 'neurons'
-python3 LSTM_train.py -e 100 -n 'neurons_5' -d 5 -s 'neurons'
+python3 LSTM_train.py -e 100 -n 'neurons_20' -d 20 -s 'neurons' --label 9
+python3 LSTM_train.py -e 100 -n 'neurons_5' -d 5 -s 'neurons' --label 9
+python3 LSTM_train.py -e 100 -n 'neurons_100' -d 100 -s 'neurons' --label 9
+python3 LSTM_train.py -e 100 -n 'neurons_50' -d 50 -s 'neurons' --label 9
