8fd5ddc038e729d713e3421628928a5d9f80932d
javier.galindos <javier.galindos@imdea.org>
2022-04-06 10:27:50 +0200

    Update tune_hp.sh

---
+++ Modeling/csv_maker.py
@@ -0,0 +1,20 @@
+import csv
+import glob
+import pandas as pd
+
+header = ["approach", "model", "dataset", "name", "MAE", "MAPE", "MASE", "train time", "inf time", "model size"]
+with open("models_csv.csv", 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+
+starting_path = "./logs"
+if __name__ == "__main__":
+    with open("models_csv.csv", 'a', encoding='UTF8') as f:
+        files = glob.glob('./logs/**/**/**/**/metrics.txt', recursive=True)
+        writer = csv.writer(f)
+        for file in files:
+            data = pd.read_csv(file, sep=',', index_col=0, engine='python')
+            to_write = [file.split('/')[2], file.split('/')[3], file.split('/')[4], file.split('/')[5],
+                        data.iloc[0, 0], data.iloc[1, 0], data.iloc[2, 0], data.iloc[3, 0], data.iloc[4, 0],
+                        data.iloc[5, 0]]
+            writer.writerow(to_write)
--- Modeling/tune_hp.sh
+++ Modeling/tune_hp.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
-python3 ConvLSTM_train.py -m 'video' -n '917/input_16' -i 64 --label 16 -i 16 --frames 8
-python3 ConvLSTM_train.py -m 'video' -n '917/input_32' -i 64 --label 16 -i 32 --frames 8
-python3 ConvLSTM_train.py -m 'video' -n '917/input_64' -i 64 --label 16 -i 64 --frames 8
-python3 ConvLSTM_train.py -m 'video' -n '917/input_128' -i 64 --label 16 -i 128 --frames 8
+python3 ConvLSTM_train.py -m 'AE' -n '917/ratio_2' -i 128 --label 16 --img_width 64
+python3 ConvLSTM_train.py -m 'AE' -n '917/ratio_3' -i 192 --label 16 --img_width 64
+python3 ConvLSTM_train.py -m 'AE' -n '917/ratio_4' -i 256 --label 16 --img_width 64
+python3 ConvLSTM_train.py -m 'AE' -n '917/ratio_5' -i 320 --label 16 --img_width 64
+python3 ConvLSTM_train.py -m 'video' -n '917/ratio_1' -i 128 --label 16 --img_width 64
+python3 ConvLSTM_train.py -m 'video' -n '917/ratio_2' -i 128 --label 16 --img_width 64
+python3 ConvLSTM_train.py -m 'video' -n '917/ratio_3' -i 192 --label 16 --img_width 64
+python3 ConvLSTM_train.py -m 'video' -n '917/ratio_4' -i 256 --label 16 --img_width 64
+python3 ConvLSTM_train.py -m 'video' -n '917/ratio_5' -i 320 --label 16 --img_width 64
 
 
 
