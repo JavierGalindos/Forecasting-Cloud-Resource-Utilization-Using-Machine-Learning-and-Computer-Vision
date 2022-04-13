3ecb3b386388eae67e8ac0f55fc6975a03a36b17
javier.galindos <javier.galindos@imdea.org>
2022-04-13 12:11:45 +0200

    Update tune_hp.sh

--- Modeling/hp_barplot.py
+++ Modeling/hp_barplot.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Constants
-LOGS_PATH = './logs/ConvLSTM/AE/917'
-HP = 'image'
-HP_NAME = 'image'
+LOGS_PATH = './logs/ConvLSTM/AE/917/Barplot'
+HP = 'input'
+HP_NAME = 'input'
 FIGURES_PATH = '../Figures/Modeling/ConvLSTM/AE/917'
+plt.rcParams['font.size'] = '13'
 if __name__ == "__main__":
     # List files
     hp_list =[]
@@ -18,19 +19,24 @@ if __name__ == "__main__":
     hp_df_list = []
     for idx, file in enumerate(hp_list):
         # Remove int() if parameters are not numeric
-        # data = pd.read_csv(os.path.join(LOGS_PATH, hp_list[idx], 'metrics.txt'),
-        #                    names=[int(hp_list[idx].split('_')[1])], sep=',', index_col=0, engine='python')
         data = pd.read_csv(os.path.join(LOGS_PATH, hp_list[idx], 'metrics.txt'),
-                            names=[hp_list[idx].split('_')[1]], sep=',', index_col=0, engine='python')
+                           names=[int(hp_list[idx].split('_')[1])], sep=',', index_col=0, engine='python')
+        # data = pd.read_csv(os.path.join(LOGS_PATH, hp_list[idx], 'metrics.txt'),
+        #                     names=[hp_list[idx].split('_')[1]], sep=',', index_col=0, engine='python')
         hp_df_list.append(data)
 
     # Create dataframe and sort
     hp_df = pd.concat(hp_df_list, axis=1)
     hp_df.sort_index(axis=1, inplace=True)
+    hp_df = hp_df.T
+    hp_df['model_size [B]'] = hp_df['model_size [B]'] * 0.000001
+    hp_df = hp_df.rename(columns={'model_size [B]': 'model_size [MB]'})
+    hp_df = hp_df.T
+    print(hp_df.round(3).T)
 
     # Generate figures and save them
     # MAE, MAPE & RMSE
-    fig = plt.figure(dpi=200)
+    fig = plt.figure(dpi=200, figsize=(5, 3))
     hp_df.iloc[1:4, :].plot.bar(rot=0)
     plt.xlabel('Metric')
     plt.title(f'Hyperparameter: {HP_NAME}')
@@ -41,8 +47,8 @@ if __name__ == "__main__":
     plt.close(fig)
 
     # Training time
-    fig = plt.figure(dpi=200)
-    hp_df.iloc[5, :].plot.bar(rot=0)
+    fig = plt.figure(dpi=200, figsize=(5, 3))
+    hp_df.iloc[5, :].plot.bar(rot=0, width=0.2)
     plt.xlabel(f'{HP_NAME}')
     plt.ylabel('Training time [s]')
     plt.title(f'Hyperparameter: {HP_NAME}')
@@ -53,8 +59,8 @@ if __name__ == "__main__":
     plt.close(fig)
 
     # Inference time
-    fig = plt.figure(dpi=200)
-    hp_df.iloc[6, :].plot.bar(rot=0, color='tab:orange')
+    fig = plt.figure(dpi=200, figsize=(5, 3))
+    hp_df.iloc[6, :].plot.bar(rot=0, width=0.2, color='tab:orange')
     plt.xlabel(f'{HP_NAME}')
     plt.ylabel('Inference time [s]')
     plt.title(f'Hyperparameter: {HP_NAME}')
@@ -65,8 +71,8 @@ if __name__ == "__main__":
     plt.close(fig)
 
     # Size of the mode
-    fig = plt.figure(dpi=200)
-    hp_df.iloc[7, :].plot.bar(rot=0, color='tab:green')
+    fig = plt.figure(dpi=200, figsize=(5, 3))
+    hp_df.iloc[7, :].plot.bar(rot=0, width=0.2, color='tab:green')
     plt.xlabel(f'{HP_NAME}')
     plt.ylabel('Size of the model [B]')
     plt.title(f'Hyperparameter: {HP_NAME}')
@@ -77,8 +83,8 @@ if __name__ == "__main__":
     plt.close(fig)
 
     # IoU
-    fig = plt.figure(dpi=200)
-    hp_df.iloc[8, :].plot.bar(rot=0, color='tab:purple')
+    fig = plt.figure(dpi=200, figsize=(5, 3))
+    hp_df.iloc[8, :].plot.bar(rot=0, width=0.2, color='tab:purple')
     plt.xlabel(f'{HP_NAME}')
     plt.ylabel('IoU')
     plt.title(f'Hyperparameter: {HP_NAME}')
@@ -89,8 +95,8 @@ if __name__ == "__main__":
     plt.close(fig)
 
     # DTW
-    fig = plt.figure(dpi=200)
-    hp_df.iloc[9, :].plot.bar(rot=0, color='tab:red')
+    fig = plt.figure(dpi=200, figsize=(5, 3))
+    hp_df.iloc[9, :].plot.bar(rot=0, width=0.2, color='tab:red')
     plt.xlabel(f'{HP_NAME}')
     plt.ylabel('Dynamic time warping')
     plt.title(f'Hyperparameter: {HP_NAME}')
--- Modeling/tune_hp.sh
+++ Modeling/tune_hp.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-python3 ConvLSTM_train.py -m 'AE' -n '917/input_64' -i 64 --label 16 --img_width 64 --VM 917
-python3 ConvLSTM_train.py -m 'AE' -n '917/input_100' -i 100 --label 16 --img_width 128 --VM 917
-python3 ConvLSTM_train.py -m 'AE' -n '917/input_128' -i 128 --label 16 --img_width 128 --VM 917
-python3 ConvLSTM_train.py -m 'AE' -n '917/input_256' -i 256 --label 16 --img_width 256 --VM 917
+python3 ConvLSTM_train.py -m 'AE' -n '917/ratio_1' -i 64 --label 16 --img_width 64 --VM 917
+python3 ConvLSTM_train.py -m 'AE' -n '917/ratio_2' -i 128 --label 16 --img_width 64 --VM 917
+python3 ConvLSTM_train.py -m 'AE' -n '917/ratio_3' -i 192 --label 16 --img_width 64 --VM 917
+python3 ConvLSTM_train.py -m 'AE' -n '917/ratio_4' -i 256 --label 16 --img_width 64 --VM 917
 
 
 
