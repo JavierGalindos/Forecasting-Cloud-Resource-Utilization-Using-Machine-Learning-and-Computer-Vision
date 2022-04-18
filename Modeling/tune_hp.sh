5d96ea58441816b6317820363a7f2d2680a2715a
javier.galindos <javier.galindos@imdea.org>
2022-04-18 11:15:36 +0200

    Update tune_hp.sh

--- Modeling/tune_hp.sh
+++ Modeling/tune_hp.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
-python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_50' -i 32 --VM 917 --overlapping 0.50
-python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_75' -i 64 --VM 917 --overlapping 0.75
-python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_80' -i 80 --VM 917 --overlapping 0.80
-python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_90' -i 160 --VM 917 --overlapping 0.90
-python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_95' -i 320 --VM 917 --overlapping 0.95
-python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_50' -i 32 --VM 917 --overlapping 0.50
-python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_75' -i 64 --VM 917 --overlapping 0.75
-python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_80' -i 80 --VM 917 --overlapping 0.80
-python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_90' -i 160 --VM 917 --overlapping 0.90
-python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_95' -i 320 --VM 917 --overlapping 0.95
-python3 ConvLSTM_train.py -m 'video' -n '917/overlap_50' -i 32 --VM 917 --overlapping 0.50
-python3 ConvLSTM_train.py -m 'video' -n '917/overlap_75' -i 64 --VM 917 --overlapping 0.75
-python3 ConvLSTM_train.py -m 'video' -n '917/overlap_80' -i 80 --VM 917 --overlapping 0.80
-python3 ConvLSTM_train.py -m 'video' -n '917/overlap_90' -i 160 --VM 917 --overlapping 0.90
-python3 ConvLSTM_train.py -m 'video' -n '917/overlap_95' -i 320 --VM 917 --overlapping 0.95
+python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_50' -i 32 --VM 917 --overlapping 0.50 --label 16
+python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_75' -i 64 --VM 917 --overlapping 0.75 --label 16
+python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_80' -i 80 --VM 917 --overlapping 0.80 --label 16
+python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_90' -i 160 --VM 917 --overlapping 0.90 --label 16
+python3 ConvLSTM_train.py -m 'AE' -n '917/overlap_95' -i 320 --VM 917 --overlapping 0.95 --label 16
+python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_50' -i 32 --VM 917 --overlapping 0.50 --label 16
+python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_75' -i 64 --VM 917 --overlapping 0.75 --label 16
+python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_80' -i 80 --VM 917 --overlapping 0.80 --label 16
+python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_90' -i 160 --VM 917 --overlapping 0.90 --label 16
+python3 ConvLSTM_train.py -m 'LRCN' -n '917/overlap_95' -i 320 --VM 917 --overlapping 0.95 --label 16
+python3 ConvLSTM_train.py -m 'video' -n '917/overlap_50' -i 32 --VM 917 --overlapping 0.50 --label 16
+python3 ConvLSTM_train.py -m 'video' -n '917/overlap_75' -i 64 --VM 917 --overlapping 0.75 --label 16
+python3 ConvLSTM_train.py -m 'video' -n '917/overlap_80' -i 80 --VM 917 --overlapping 0.80 --label 16
+python3 ConvLSTM_train.py -m 'video' -n '917/overlap_90' -i 160 --VM 917 --overlapping 0.90 --label 16
+python3 ConvLSTM_train.py -m 'video' -n '917/overlap_95' -i 320 --VM 917 --overlapping 0.95 --label 16
 
 
 
