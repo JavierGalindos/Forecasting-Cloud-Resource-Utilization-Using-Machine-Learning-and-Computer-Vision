7b922473ac64c591a12a6e04ffecc137f20602a9
javier.galindos <javier.galindos@imdea.org>
2022-04-18 14:17:01 +0200

    Change in LRCN model

--- Modeling/hp_barplot.py
+++ Modeling/hp_barplot.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Constants
-LOGS_PATH = './logs/ConvLSTM/LRCN/917'
-HP = 'labels'
-HP_NAME = 'labels'
-FIGURES_PATH = '../Figures/Modeling/ConvLSTM/LRCN/917'
+LOGS_PATH = './logs/ConvLSTM/video/917'
+HP = 'overlap'
+HP_NAME = 'overlap'
+FIGURES_PATH = '../Figures/Modeling/ConvLSTM/video/917'
 plt.rcParams['font.size'] = '13'
 if __name__ == "__main__":
     # List files
