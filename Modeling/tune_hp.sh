2021d5d3446b8d74a4524bf1de0c70a784031ccf
javier.galindos <javier.galindos@imdea.org>
2022-04-22 17:24:33 +0200

    Update tune_hp.sh for CM

--- Modeling/tune_hp.sh
+++ Modeling/tune_hp.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
-#python3 ConvLSTM_CM.py -m 'AE' -n '917/CM_340' -i 128 --VM 917 --label 16 --VM_test 340
-#python3 ConvLSTM_CM.py -m 'LRCN' -n '917/CM_340' -i 128 --VM 917 --label 16 --VM_test 340
-#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_340' -i 128 --VM 917 --label 16 --VM_test 340
-python3 ConvLSTM_CM.py -m 'AE' -n '917/CM_1081' -i 128 --VM 917 --label 16 --VM_test 1081
-python3 ConvLSTM_CM.py -m 'LRCN' -n '917/CM_1081' -i 128 --VM 917 --label 16 --VM_test 1081
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_1081' -i 128 --VM 917 --label 16 --VM_test 1081
-python3 ConvLSTM_CM.py -m 'AE' -n '917/CM_555' -i 128 --VM 917 --label 16 --VM_test 555
-python3 ConvLSTM_CM.py -m 'LRCN' -n '917/CM_555' -i 128 --VM 917 --label 16 --VM_test 555
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_555' -i 128 --VM 917 --label 16 --VM_test 555
-python3 ConvLSTM_CM.py -m 'AE' -n '917/CM_119' -i 128 --VM 917 --label 16 --VM_test 119
-python3 ConvLSTM_CM.py -m 'LRCN' -n '917/CM_119' -i 128 --VM 917 --label 16 --VM_test 119
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_119' -i 128 --VM 917 --label 16 --VM_test 119
+python3 ConvLSTM_CM.py -m 'AE' -n '917/CM_251' -i 128 --VM 917 --label 16 --VM_test 251
+python3 ConvLSTM_CM.py -m 'LRCN' -n '917/CM_251' -i 128 --VM 917 --label 16 --VM_test 251
+python3 ConvLSTM_CM.py -m 'video' -n '917/CM_251' -i 128 --VM 917 --label 16 --VM_test 251
+python3 ConvLSTM_CM.py -m 'AE' -n '917/CM_322' -i 128 --VM 917 --label 16 --VM_test 322
+python3 ConvLSTM_CM.py -m 'LRCN' -n '917/CM_322' -i 128 --VM 917 --label 16 --VM_test 322
+python3 ConvLSTM_CM.py -m 'video' -n '917/CM_322' -i 128 --VM 917 --label 16 --VM_test 322
+
 
 
 
