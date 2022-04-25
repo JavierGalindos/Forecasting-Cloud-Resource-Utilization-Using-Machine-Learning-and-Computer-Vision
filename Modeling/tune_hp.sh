711ea6fd911bfecc3ef82d8699b3a994e8ab7f43
javier.galindos <javier.galindos@imdea.org>
2022-04-25 12:02:07 +0200

    Update tune_hp.sh

--- Modeling/tune_hp.sh
+++ Modeling/tune_hp.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-python3 Baseline_train.py -n '917/CM_251' --label 16 --VM_test 251
-python3 Baseline_train.py -n '917/CM_119' --label 16 --VM_test 119
-python3 Baseline_train.py -n '917/CM_340' --label 16 --VM_test 340
-python3 Baseline_train.py -n '917/CM_1081' --label 16 --VM_test 1081
+python3 Baseline_train.py -n '917/CM_322' --label 16 --VM_test 322
+python3 LSTM_CM.py  -n '917/CM_322' --VM 917 --label 16 --VM_test 322
 
 
 
