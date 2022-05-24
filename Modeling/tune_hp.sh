b6deb4c56975adaf4c9b9ff61ed074ac1df7e7cf
javier.galindos <javier.galindos@imdea.org>
2022-05-24 11:42:46 +0200

    Update tune_hp.sh for CM experiments

--- Modeling/tune_hp.sh
+++ Modeling/tune_hp.sh
@@ -1,55 +1,107 @@
 #! /bin/bash
-# Sinus
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_1081' -i 128 --VM 917 --label 16 --VM_test 1081
-python3 LSTM_CM.py  -n '917/CM_1081' --VM 917 --label 16 --VM_test 1081
+## Sinus
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_1081' -i 128 --VM 917 --label 16 --VM_test 1081
+#python3 LSTM_CM.py  -n '917/CM_1081' --VM 917 --label 16 --VM_test 1081
+#python3 Baseline_train.py  -n '917/CM_1081' --VM 917 --label 16 --VM_test 1081
 #python3 ConvLSTM_CM.py -m 'video' -n '917/CM_340' -i 128 --VM 917 --label 16 --VM_test 340
 #python3 LSTM_CM.py  -n '917/CM_340' --VM 917 --label 16 --VM_test 340
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_226' -i 128 --VM 917 --label 16 --VM_test 226
-python3 LSTM_CM.py  -n '917/CM_226' --VM 917 --label 16 --VM_test 226
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_251' -i 128 --VM 917 --label 16 --VM_test 251
-python3 LSTM_CM.py  -n '917/CM_251' --VM 917 --label 16 --VM_test 251
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_599' -i 128 --VM 917 --label 16 --VM_test 599
-python3 LSTM_CM.py  -n '917/CM_599' --VM 917 --label 16 --VM_test 599
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_118' -i 128 --VM 917 --label 16 --VM_test 118
-python3 LSTM_CM.py  -n '917/CM_118' --VM 917 --label 16 --VM_test 118
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_119' -i 128 --VM 917 --label 16 --VM_test 119
-python3 LSTM_CM.py  -n '917/CM_119' --VM 917 --label 16 --VM_test 119
-python3 ConvLSTM_CM.py -m 'video' -n '917/CM_120' -i 128 --VM 917 --label 16 --VM_test 120
-python3 LSTM_CM.py  -n '917/CM_120' --VM 917 --label 16 --VM_test 120
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_226' -i 128 --VM 917 --label 16 --VM_test 226
+#python3 LSTM_CM.py  -n '917/CM_226' --VM 917 --label 16 --VM_test 226
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_251' -i 128 --VM 917 --label 16 --VM_test 251
+#python3 LSTM_CM.py  -n '917/CM_251' --VM 917 --label 16 --VM_test 251
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_599' -i 128 --VM 917 --label 16 --VM_test 599
+#python3 LSTM_CM.py  -n '917/CM_599' --VM 917 --label 16 --VM_test 599
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_118' -i 128 --VM 917 --label 16 --VM_test 118
+#python3 LSTM_CM.py  -n '917/CM_118' --VM 917 --label 16 --VM_test 118
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_119' -i 128 --VM 917 --label 16 --VM_test 119
+#python3 LSTM_CM.py  -n '917/CM_119' --VM 917 --label 16 --VM_test 119
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_120' -i 128 --VM 917 --label 16 --VM_test 120
+#python3 LSTM_CM.py  -n '917/CM_120' --VM 917 --label 16 --VM_test 120
+## Up down
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_1081' -i 128 --VM 226 --label 16 --VM_test 1081
+#python3 LSTM_CM.py  -n '226/CM_1081' --VM 226 --label 16 --VM_test 1081
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_340' -i 128 --VM 226 --label 16 --VM_test 340
+#python3 LSTM_CM.py  -n '226/CM_340' --VM 226 --label 16 --VM_test 340
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_917' -i 128 --VM 226 --label 16 --VM_test 917
+#python3 LSTM_CM.py  -n '226/CM_917' --VM 226 --label 16 --VM_test 917
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_251' -i 128 --VM 226 --label 16 --VM_test 251
+#python3 LSTM_CM.py  -n '226/CM_251' --VM 226 --label 16 --VM_test 251
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_599' -i 128 --VM 226 --label 16 --VM_test 599
+#python3 LSTM_CM.py  -n '226/CM_599' --VM 226 --label 16 --VM_test 599
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_118' -i 128 --VM 226 --label 16 --VM_test 118
+#python3 LSTM_CM.py  -n '226/CM_118' --VM 226 --label 16 --VM_test 118
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_119' -i 128 --VM 226 --label 16 --VM_test 119
+#python3 LSTM_CM.py  -n '226/CM_119' --VM 226 --label 16 --VM_test 119
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_120' -i 128 --VM 226 --label 16 --VM_test 120
+#python3 LSTM_CM.py  -n '226/CM_120' --VM 226 --label 16 --VM_test 120
+## Weird
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_1081' -i 128 --VM 118 --label 16 --VM_test 1081
+#python3 LSTM_CM.py  -n '118/CM_1081' --VM 118 --label 16 --VM_test 1081
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_340' -i 128 --VM 118 --label 16 --VM_test 340
+#python3 LSTM_CM.py  -n '118/CM_340' --VM 118 --label 16 --VM_test 340
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_917' -i 128 --VM 118 --label 16 --VM_test 917
+#python3 LSTM_CM.py  -n '118/CM_917' --VM 118 --label 16 --VM_test 917
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_251' -i 128 --VM 118 --label 16 --VM_test 251
+#python3 LSTM_CM.py  -n '118/CM_251' --VM 118 --label 16 --VM_test 251
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_599' -i 128 --VM 118 --label 16 --VM_test 599
+#python3 LSTM_CM.py  -n '118/CM_599' --VM 118 --label 16 --VM_test 599
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_226' -i 128 --VM 118 --label 16 --VM_test 226
+#python3 LSTM_CM.py  -n '118/CM_226' --VM 118 --label 16 --VM_test 226
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_119' -i 128 --VM 118 --label 16 --VM_test 119
+#python3 LSTM_CM.py  -n '118/CM_119' --VM 118 --label 16 --VM_test 119
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_120' -i 128 --VM 118 --label 16 --VM_test 120
+#python3 LSTM_CM.py  -n '118/CM_120' --VM 118 --label 16 --VM_test 120
+
+# Exp smooth
+# Sinus
+
+python3 Baseline_train.py  -n '917/CM_1081' --VM 917 --label 16 --VM_test 1081
+python3 Baseline_train.py  -n '917/CM_340' --VM 917 --label 16 --VM_test 340
+python3 Baseline_train.py  -n '917/CM_226' --VM 917 --label 16 --VM_test 226
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_251' -i 128 --VM 917 --label 16 --VM_test 251
+python3 Baseline_train.py  -n '917/CM_251' --VM 917 --label 16 --VM_test 251
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_599' -i 128 --VM 917 --label 16 --VM_test 599
+python3 Baseline_train.py  -n '917/CM_599' --VM 917 --label 16 --VM_test 599
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_118' -i 128 --VM 917 --label 16 --VM_test 118
+python3 Baseline_train.py  -n '917/CM_118' --VM 917 --label 16 --VM_test 118
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_119' -i 128 --VM 917 --label 16 --VM_test 119
+python3 Baseline_train.py  -n '917/CM_119' --VM 917 --label 16 --VM_test 119
+#python3 ConvLSTM_CM.py -m 'video' -n '917/CM_120' -i 128 --VM 917 --label 16 --VM_test 120
+python3 Baseline_train.py  -n '917/CM_120' --VM 917 --label 16 --VM_test 120
 # Up down
-python3 ConvLSTM_CM.py -m 'video' -n '226/CM_1081' -i 128 --VM 226 --label 16 --VM_test 1081
-python3 LSTM_CM.py  -n '226/CM_1081' --VM 226 --label 16 --VM_test 1081
-python3 ConvLSTM_CM.py -m 'video' -n '226/CM_340' -i 128 --VM 226 --label 16 --VM_test 340
-python3 LSTM_CM.py  -n '226/CM_340' --VM 226 --label 16 --VM_test 340
-python3 ConvLSTM_CM.py -m 'video' -n '226/CM_917' -i 128 --VM 226 --label 16 --VM_test 917
-python3 LSTM_CM.py  -n '226/CM_917' --VM 226 --label 16 --VM_test 917
-python3 ConvLSTM_CM.py -m 'video' -n '226/CM_251' -i 128 --VM 226 --label 16 --VM_test 251
-python3 LSTM_CM.py  -n '226/CM_251' --VM 226 --label 16 --VM_test 251
-python3 ConvLSTM_CM.py -m 'video' -n '226/CM_599' -i 128 --VM 226 --label 16 --VM_test 599
-python3 LSTM_CM.py  -n '226/CM_599' --VM 226 --label 16 --VM_test 599
-python3 ConvLSTM_CM.py -m 'video' -n '226/CM_118' -i 128 --VM 226 --label 16 --VM_test 118
-python3 LSTM_CM.py  -n '226/CM_118' --VM 226 --label 16 --VM_test 118
-python3 ConvLSTM_CM.py -m 'video' -n '226/CM_119' -i 128 --VM 226 --label 16 --VM_test 119
-python3 LSTM_CM.py  -n '226/CM_119' --VM 226 --label 16 --VM_test 119
-python3 ConvLSTM_CM.py -m 'video' -n '226/CM_120' -i 128 --VM 226 --label 16 --VM_test 120
-python3 LSTM_CM.py  -n '226/CM_120' --VM 226 --label 16 --VM_test 120
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_1081' -i 128 --VM 226 --label 16 --VM_test 1081
+python3 Baseline_train.py  -n '226/CM_1081' --VM 226 --label 16 --VM_test 1081
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_340' -i 128 --VM 226 --label 16 --VM_test 340
+python3 Baseline_train.py  -n '226/CM_340' --VM 226 --label 16 --VM_test 340
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_917' -i 128 --VM 226 --label 16 --VM_test 917
+python3 Baseline_train.py  -n '226/CM_917' --VM 226 --label 16 --VM_test 917
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_251' -i 128 --VM 226 --label 16 --VM_test 251
+python3 Baseline_train.py  -n '226/CM_251' --VM 226 --label 16 --VM_test 251
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_599' -i 128 --VM 226 --label 16 --VM_test 599
+python3 Baseline_train.py  -n '226/CM_599' --VM 226 --label 16 --VM_test 599
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_118' -i 128 --VM 226 --label 16 --VM_test 118
+python3 Baseline_train.py  -n '226/CM_118' --VM 226 --label 16 --VM_test 118
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_119' -i 128 --VM 226 --label 16 --VM_test 119
+python3 Baseline_train.py  -n '226/CM_119' --VM 226 --label 16 --VM_test 119
+#python3 ConvLSTM_CM.py -m 'video' -n '226/CM_120' -i 128 --VM 226 --label 16 --VM_test 120
+python3 Baseline_train.py  -n '226/CM_120' --VM 226 --label 16 --VM_test 120
 # Weird
-python3 ConvLSTM_CM.py -m 'video' -n '118/CM_1081' -i 128 --VM 118 --label 16 --VM_test 1081
-python3 LSTM_CM.py  -n '118/CM_1081' --VM 118 --label 16 --VM_test 1081
-python3 ConvLSTM_CM.py -m 'video' -n '118/CM_340' -i 128 --VM 118 --label 16 --VM_test 340
-python3 LSTM_CM.py  -n '118/CM_340' --VM 118 --label 16 --VM_test 340
-python3 ConvLSTM_CM.py -m 'video' -n '118/CM_917' -i 128 --VM 118 --label 16 --VM_test 917
-python3 LSTM_CM.py  -n '118/CM_917' --VM 118 --label 16 --VM_test 917
-python3 ConvLSTM_CM.py -m 'video' -n '118/CM_251' -i 128 --VM 118 --label 16 --VM_test 251
-python3 LSTM_CM.py  -n '118/CM_251' --VM 118 --label 16 --VM_test 251
-python3 ConvLSTM_CM.py -m 'video' -n '118/CM_599' -i 128 --VM 118 --label 16 --VM_test 599
-python3 LSTM_CM.py  -n '118/CM_599' --VM 118 --label 16 --VM_test 599
-python3 ConvLSTM_CM.py -m 'video' -n '118/CM_226' -i 128 --VM 118 --label 16 --VM_test 226
-python3 LSTM_CM.py  -n '118/CM_226' --VM 118 --label 16 --VM_test 226
-python3 ConvLSTM_CM.py -m 'video' -n '118/CM_119' -i 128 --VM 118 --label 16 --VM_test 119
-python3 LSTM_CM.py  -n '118/CM_119' --VM 118 --label 16 --VM_test 119
-python3 ConvLSTM_CM.py -m 'video' -n '118/CM_120' -i 128 --VM 118 --label 16 --VM_test 120
-python3 LSTM_CM.py  -n '118/CM_120' --VM 118 --label 16 --VM_test 120
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_1081' -i 128 --VM 118 --label 16 --VM_test 1081
+python3 Baseline_train.py  -n '118/CM_1081' --VM 118 --label 16 --VM_test 1081
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_340' -i 128 --VM 118 --label 16 --VM_test 340
+python3 Baseline_train.py  -n '118/CM_340' --VM 118 --label 16 --VM_test 340
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_917' -i 128 --VM 118 --label 16 --VM_test 917
+python3 Baseline_train.py  -n '118/CM_917' --VM 118 --label 16 --VM_test 917
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_251' -i 128 --VM 118 --label 16 --VM_test 251
+python3 Baseline_train.py  -n '118/CM_251' --VM 118 --label 16 --VM_test 251
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_599' -i 128 --VM 118 --label 16 --VM_test 599
+python3 Baseline_train.py  -n '118/CM_599' --VM 118 --label 16 --VM_test 599
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_226' -i 128 --VM 118 --label 16 --VM_test 226
+python3 Baseline_train.py  -n '118/CM_226' --VM 118 --label 16 --VM_test 226
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_119' -i 128 --VM 118 --label 16 --VM_test 119
+python3 Baseline_train.py  -n '118/CM_119' --VM 118 --label 16 --VM_test 119
+#python3 ConvLSTM_CM.py -m 'video' -n '118/CM_120' -i 128 --VM 118 --label 16 --VM_test 120
+python3 Baseline_train.py  -n '118/CM_120' --VM 118 --label 16 --VM_test 120
 
 
 
