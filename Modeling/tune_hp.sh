7543dc3dc8825ee63cdc35c172f11011cc81188a
javier.galindos <javier.galindos@imdea.org>
2022-05-30 12:46:47 +0200

    Create CM for every VM (LSTM) & update script

--- Modeling/tune_hp.sh
+++ Modeling/tune_hp.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#python3 ConvLSTM_CM_allVM.py -m 'video' -n '917/CM917/1' -i 128 --VM 917 --label 16
+python3 ConvLSTM_CM_allVM.py -m 'video' -n '917/CM917/1' -i 128 --VM 917 --label 16
 python3 ConvLSTM_CM_allVM.py -m 'video' -n '917/CM118/1' -i 128 --VM 118 --label 16
 python3 ConvLSTM_CM_allVM.py -m 'video' -n '917/CM226/1' -i 128 --VM 226 --label 16
 python3 LSTM_CM_allVM.py -n '917/CM917/1' -i 128 --VM 917 --label 16
